7ac3bcb92b1cbdb256c7334603ebc517a578bcd0
Garth Madden <garthmadden809@gmail.com>
2020-08-20 12:00:06 -0500

    Update FindMissing.py

--- FindMissing.py
+++ FindMissing.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
-#GARTH MADDEN 2020 Garthmadden809@gmail.com
+
 #FINDS MISSING BLOCKS IN DATABASE
+#    Copyright (C) <2020>  <Garth Madden, Garthmadden809@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import pymongo
 from web3 import Web3
